4628cd089a22a5db78a57201338d459544452672
Jordan Fair <149626196+jjbos5@users.noreply.github.com>
2026-07-10 16:07:58 -0400

    feat: Simulates shipping bleed

--- src/simulations.py
+++ src/simulations.py
@@ -1,2 +1,6 @@
 def simulations():
     print ("this is a test")
+
+def net_bleed(df):
+    shipping_calc = (df["shipping_charged"] - df["shipping_cost"]).sum() 
+    return 
--- tests/test_simulations.py
+++ tests/test_simulations.py
@@ -1,4 +1,11 @@
 import src.simulations as sim
+import pandas as pd
 
 def test_simulation_is_callable():
     assert callable(sim.simulations)
+
+def test_where_we_stand_with_shipping():
+    test1 = pd.DataFrame({"shipping_charged": [12.0, 16.50, 6.00],
+        "shipping_cost":[15.00, 22.00, 5.00] })
+    result = sim.net_bleed(test1)
+    assert result == result
