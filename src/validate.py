aba4583cbe996ed0fd01c9af64dc24a2baefabb7
Jordan Fair <149626196+jjbos5@users.noreply.github.com>
2026-07-16 11:54:01 -0400

    feat: Validate negatice shipping cost

--- src/validate.py
+++ src/validate.py
@@ -1,4 +1,9 @@
 def count_missing_costs(df):
     missing = df[df["shipping_cost"] ==0]
     missing_counts = len(missing)
-    return missing_counts
+    return missing_counts 
+
+def negative_shipping_costs(df):
+    negative = df[df["shipping_cost"] <0]
+    negative_shipping = len(negative)
+    return negative_shipping
--- tests/test_validate.py
+++ tests/test_validate.py
@@ -6,3 +6,9 @@ def test_counts_missing_cost():
         "shipping_cost": [45.00, 0.00, 12.00, 0.00]})
     result = val.count_missing_costs(missing)
     assert result == 2
+
+def test_negative_shipping_cost():
+    negative = pd.DataFrame({ "shipping_charged": [12.00, 0.00, 15.00, 8.00],
+        "shipping_cost": [12.00, -5.00, 15.00, 8.00]})
+    result = val.negative_shipping_costs(negative)
+    assert result == 1 
